871972f5ab348f5ea07efa548b9ae3082e7af7d4
dgornin <dgornin@gmail.com>
2022-09-29 02:09:14 +0300

    p11

---
+++ p11/src/useMapbox.js
@@ -0,0 +1,24 @@
+import { useLayoutEffect } from "react";
+import mapboxgl from "mapbox-gl";
+
+export default function useMapbox(container, center, zoom) {
+    mapboxgl.accessToken =
+    "pk.eyJ1Ijoib3RyaWNhbGEiLCJhIjoiY2tqMjFjcTNzMnp2djJ1bW05NW9zd2h6NCJ9.0w535oE4G9JjDpDxC_vVuw";
+    if (!container) {
+        container = "map";
+    }
+    if (!center) {
+        center = [37.61192, 55.76199];
+    }
+    if (!zoom) {
+        zoom = 10
+    }
+    useLayoutEffect(() => {
+        const map = new mapboxgl.Map({
+          container: container,
+          style: "mapbox://styles/mapbox/dark-v10",
+          center: center,
+          zoom: zoom
+        });
+    }, []);
+}
